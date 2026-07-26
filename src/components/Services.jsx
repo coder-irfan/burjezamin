7ced94b38ad64fa1bbc30730fa3db15455a6f6f7
Mohammad Irfan <irfan.noorzada123@gmail.com>
2026-07-26 20:59:39 +0430

    fixed css

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -105,7 +105,7 @@ function Services({ getDirection }) {
                 </div>
 
                 <div className="p-6 flex flex-col flex-1 space-y-2 relative">
-                  <span className="absolute -top-8 right-4 text-6xl font-bold text-colors-buttonBg/60">
+                  <span className="absolute -top-8 right-4 text-6xl font-bold text-colors-buttonBg/70">
                     0{index + 1}
                   </span>
 
